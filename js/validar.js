bc200e0a87455061d2b081d046376f5344187b88
andres <36251673+Cachasa6@users.noreply.github.com>
2018-11-01 09:14:39 -0500

    validacion formulario con javasript

---
+++ js/validar.js
@@ -0,0 +1,68 @@
+function validacion() {
+	
+	indice = document.getElementById("tipodoc").selectedIndex;
+	
+	
+	if( indice == null || indice == 0 ) {
+		document.getElementById("tipodoc").style="border:thin solid red";
+		alert("Por favor no ha seleccionado un tipo de documento por favor seleccione un opcion");
+			
+  return false;
+}
+
+doc = document.getElementById("identificacion").value;
+if( isNaN(doc) ) {
+	alert("No de documento Campo vacio o con letras por favor solo digite numeros"); 
+	document.getElementById("identificacion").style="border:thin solid red";
+  return false;
+}
+
+nombre = document.getElementById("nombres").value;
+if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
+		alert("Nombres Campo vacio por favor diligencie el campo");
+		document.getElementById("nombres").style="border:thin solid red";
+  return false;
+}
+
+
+apellidouno = document.getElementById("apellido1").value;
+if( apellidouno == null || apellidouno.length == 0 || /^\s+$/.test(apellidouno) ) {
+		alert(" Apellido 1 Campo vacio por favor diligencie el campo");
+		document.getElementById("apellido1").style="border:thin solid red";
+  return false;
+}
+
+apellidodos = document.getElementById("apellido2").value;
+if( apellidodos == null || apellidodos.length == 0 || /^\s+$/.test(apellidodos) ) {
+		alert(" Apellido 2 Campo vacio por favor diligencie el campo");
+		document.getElementById("apellido2").style="border:thin solid red";
+  return false;
+}
+
+
+var ano = document.getElementById("fechanac").value;
+var mes = document.getElementById("fechanac").value;
+var dia = document.getElementById("fechanac").value;
+
+
+
+ 
+fecha = new Date(ano, mes, dia);
+ 
+if( !isNaN(fecha) ) {
+	alert(" Fecha de nacimento Campo vacio por favor diligencie el campo");
+	
+	
+	document.getElementById("fechanac").style="border:thin solid red";
+  return false;
+}
+
+
+
+
+
+
+
+  return true;
+  
+}
